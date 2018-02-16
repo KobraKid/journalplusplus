78f462cf992de35443bfb0468a611d19aa067862
Michael Huyler <michaelhuyler2020@u.northwestern.edu>
2018-02-16 10:56:47 -0600

    updating master branch.

--- public-html/js/script.js
+++ public-html/js/script.js
@@ -1,7 +1,25 @@
 var title = "BuJo++";
+var profileVisible = false;
+var profileOptions;
 
 function init() {
 	document.title = title;
+	if (document.getElementById("profile") !== null) {
+		document.getElementById("profile").addEventListener('click', toggleProfile);;
+		profileOptions = document.getElementById("profile-dropdown");
+		profileOptions.style.display = "none";
+	}
+}
+
+function toggleProfile() {
+	if (profileOptions === null) return;
+	
+	profileVisible = !profileVisible;
+	if (profileVisible) {
+		profileOptions.style.display = "block";
+	} else {
+		profileOptions.style.display = "none";		
+	}
 }
 
 init();
